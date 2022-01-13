eecfeca6ca46dc9360400191ab44b41221f1698b
Yuliya <vsggdfaf@gmail.com>
2022-01-13 14:39:58 +0300

    добавление const в js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//todo-control
+//header-input
+//todo-list
+//todo-completed
+
+const todoControl = document.querySelector('.todo-control')
+const headerInput = document.querySelector('.header-input')
+const todoList = document.querySelector('.todo-list')
+const todoCompleted = document.querySelector('.todo-completed')
