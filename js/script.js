db03a6894e5663643d2cdd2492b198674061b6da
Yuliya <vsggdfaf@gmail.com>
2022-01-13 15:09:15 +0300

    добавление дел и вывод их в консоль

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ const todoControl = document.querySelector('.todo-control')
 const headerInput = document.querySelector('.header-input')
 const todoList = document.querySelector('.todo-list')
 const todoCompleted = document.querySelector('.todo-completed')
+
+const toDoData = [
+    {
+        text: 'Сварить кофе',
+        completed: false
+    },
+    {
+        text: 'Помыть посуду',
+        completed: true
+    }
+]
+
+const render = function () {
+    console.log(toDoData);
+}
+
+todoControl.addEventListener('submit', function (event) {
+    event.preventDefault()
+    
+    const newToDo = {
+        text: headerInput.value,
+        completed: false
+    }
+
+    toDoData.push(newToDo)
+    //очистить input
+    headerInput.value = ''
+
+    render()
+})
